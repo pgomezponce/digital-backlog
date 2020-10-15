c3d66f35a0589a5ebd786347dca1c79a75768863
Pablo Gómez Ponce <pgomezponce@gmail.com>
2020-10-16 00:35:44 +0200

    Added Eva Icons

--- App.tsx
+++ App.tsx
@@ -1,4 +1,4 @@
-import { ApplicationProvider, Layout } from "@ui-kitten/components";
+import { ApplicationProvider, IconRegistry, Layout } from "@ui-kitten/components";
 import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { SafeAreaProvider } from "react-native-safe-area-context";
@@ -8,6 +8,7 @@ import useColorScheme from "./hooks/useColorScheme";
 import Navigation from "./navigation";
 
 import * as eva from "@eva-design/eva";
+import { EvaIconsPack } from '@ui-kitten/eva-icons';
 
 import store from "./redux/store";
 import { Provider } from "react-redux";
@@ -30,6 +31,8 @@ export default function App() {
   } else {
     return (
       <Provider store={store}>
+        <IconRegistry icons={EvaIconsPack} />
+
         <ApplicationProvider {...eva} theme={eva.light}>
           <SafeAreaProvider>
             <Navigation colorScheme={colorScheme} />
